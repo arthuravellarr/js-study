a3a65465475f0dc9aa45b847821e4e1b7b39d399
Arthur Avelar <arthur.avelar@outlook.com.br>
2025-05-15 21:16:51 -0300

    finalizando os scripts a acertando posicionamento dos elementos do modal

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-var message = window.document.getElementById('idMsg')
-var image = window.document.getElementById('idImg')
+function load() {
+    var message = window.document.getElementById('idMsg')
+    var image = window.document.getElementById('idImg')   
 
+    var date = new Date()
+    var hour = date.getHours()
+    message.innerHTML = `It's ${hour}h now`
+
+    if (hour >= 4 && hour < 12) {
+        image.src = 'imgs/img-morning.png'
+        document.body.style.background = '#d1a858'
+    } else if (hour >= 12 && hour < 18) {
+        image.src = 'imgs/img-afternoon.png'
+        document.body.style.background = '#d1a858'
+    } else {
+        image.src = 'imgs/img-night.png'
+        document.body.style.background = '#0d3661'
+    }
+}
