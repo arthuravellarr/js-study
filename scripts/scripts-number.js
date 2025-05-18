497fcdfc105fe52e0b1617bd5bbd83495b11e19a
Arthur Avelar <arthur.avelar@outlook.com.br>
2025-05-18 18:22:16 -0300

    Finalizando a ultima pagina

--- scripts/scripts-number.js
+++ scripts/scripts-number.js
@@ -11,10 +11,49 @@ function inList(n, l) {
     return (l.indexOf(Number(n)) != -1)
 }
 
+function showOnSelect(n) {
+    let item = document.createElement('option')
+    item.text = `${n} added to list`
+    item.value = `tab${values.indexOf(Number(n))}`
+    list.appendChild(item)
+}
+ 
 function add() {
-    if (isNumber(num.value) && !inList(num.value, list)) {
-
+    if (isNumber(num.value) && !inList(num.value, values)) {
+        values.push(Number(num.value))
+        showOnSelect(num.value)
+        result.innerHTML = ''
     } else {
-        window.alert('Invalid!')
+        window.alert('Invalid or already added to the list!')
+    }
+
+    num.value = ''
+    num.focus()
+}
+
+function sumValues(array) {
+    let total = 0
+    for (let n in array) {
+        total += array[n]
+    }
+
+    return total
+}
+
+function finalize() {
+    if (values.length === 0) {
+        window.alert('No data to analyze, add some number!')
+    } else {
+        let total = values.length
+        let [max, min] = [Math.max(...values), Math.min(...values)]
+        let average = sumValues(values) / total
+
+        result.innerHTML = ''
+        result.innerHTML += `<p><strong>${total}</strong> numbers added</p>`
+        result.innerHTML += `<p>The biggest number is <strong>${max}</strong>, and smaller is <strong>${min}</strong></p>`
+        result.innerHTML += `<p>The sum numbers is <strong>${sumValues(values)}</strong></p>`
+        result.innerHTML += `<p>The average is <strong>${average}</strong></p>`
+
+        result.style.display = 'block'
     }
 }
