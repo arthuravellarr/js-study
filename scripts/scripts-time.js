e934aceb30ba79780c85931f0939a22e1256bbfd
Arthur Avelar <arthur.avelar@outlook.com.br>
2025-05-18 23:58:05 -0300

    Refatorando o time para mostrar a hora em ingles e alterando o background

--- scripts/scripts-time.js
+++ scripts/scripts-time.js
@@ -3,17 +3,27 @@ function load() {
     var image = window.document.getElementById('idImg')   
 
     var date = new Date()
-    var hour = date.getHours()
-    message.innerHTML = `It's ${hour}h now`
+    var timeStamp = new Intl.DateTimeFormat('en-us', {timeStyle: "short"}).format(date)
+    message.innerHTML = `It's <strong>${timeStamp}</strong> now`
 
-    if (hour >= 4 && hour < 12) {
-        image.src = '../imgs/img-morning.png'
-        document.body.style.background = '#d1a858'
-    } else if (hour >= 12 && hour < 18) {
-        image.src = '../imgs/img-afternoon.png'
-        document.body.style.background = '#d1a858'
+    var hour = Number(timeStamp.split(' ')[0].split(':')[0])
+    var period = timeStamp.split(' ')[1]
+
+    if (period === 'AM') {
+        if (hour >= 4) {
+            image.src = '../imgs/img-morning.png'
+            document.body.style.background = 'linear-gradient(7deg,rgba(255, 243, 185, 1) 29%, rgba(255, 157, 160, 1) 100%)'
+        } else {
+            image.src = '../imgs/img-night.png'
+            document.body.style.background = 'linear-gradient(7deg,rgb(23, 81, 143) 0%, rgba(13, 54, 97, 1) 40%, rgba(0, 0, 0, 1) 100%)'
+        }
     } else {
-        image.src = '../imgs/img-night.png'
-        document.body.style.background = '#0d3661'
+        if (hour < 6) {
+            image.src = '../imgs/img-afternoon.png'
+            document.body.style.background = 'linear-gradient(7deg,rgba(209, 168, 88, 1) 8%, rgba(255, 157, 160, 1) 65%, rgba(206, 129, 181, 1) 100%)'
+        } else {
+            image.src = '../imgs/img-night.png'
+            document.body.style.background = 'linear-gradient(7deg,rgb(23, 81, 143) 0%, rgba(13, 54, 97, 1) 40%, rgba(0, 0, 0, 1) 100%)'
+        }
     }
 }
